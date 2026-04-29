de5674c406df155031edff2361d4f04939ec11c4
Sveta <ma1m2@mail.ru>
2026-04-29 22:04:07 +0300

    add CustomAllureListener

---
+++ src/test/java/msl/qa/allure/CustomAllureListener.java
@@ -0,0 +1,16 @@
+package msl.qa.allure;
+
+import io.qameta.allure.restassured.AllureRestAssured;
+
+public class CustomAllureListener {
+
+  private static final AllureRestAssured FILTER = new AllureRestAssured();
+
+  public static AllureRestAssured withCustomTemplate() {
+    FILTER.setRequestTemplate("request.ftl");
+    FILTER.setResponseTemplate("response.ftl");
+
+    return FILTER;
+  }
+
+}
